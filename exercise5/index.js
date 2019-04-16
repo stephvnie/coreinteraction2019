8d5df6351d2763ebdcada11db3d3feedf9df6b1d
stephvnie <46946099+stephvnie@users.noreply.github.com>
2019-04-16 16:49:22 -0400

    Update index.js

--- exercise5/index.js
+++ exercise5/index.js
@@ -1,54 +1,80 @@
 $(document).ready(function(){
 
-    var width = $(document).width() - 300;
-    var height = $(document).height() - 300;
 
-    console.log(width)
+    var txt = "yo hey"
+    var age = " welcome to"
+    var combined = txt + age;
+        combined += " exercise 5";
+    var number = -1;
 
-$('#circle').hover(function(){
+    var colors = ['#FDC7D7','#FF9DE6','#A5D8F3','#E8E500'];
+    var size = ['20px','100px','50px','13px','35px'];
 
-    var randomNumber = Math.random() * 300;
-    var randomRadius = randomNumber + "px"
+    var heys = ["jk go away", "i love coding", "welcome to exercise 5", "bye bye"]
 
-    var randomNumber2 = Math.random() * 360;
-    var randomHue = "hue-rotate(" + randomNumber2 + "deg)"
-
-    var randomNumber3 = Math.random() * width;
-    var randomLeft = randomNumber3 + "px"
-
-    var randomNumber4 = Math.random() * height;
-    var randomTop = randomNumber4 + "px"
-
-    var randomOpacity = Math.random() * .5 + .5;
+    var colors1 = ['#08F7FE','#09FBD3','#FE53BB','#F5D300'];
 
 
-    $(this).css('border-radius', randomRadius);
-    $(this).css('filter', randomHue);
-    $(this).css('left', randomLeft);
-    $(this).css('top', randomTop);
-    $(this).css('opacity', randomOpacity);
 
-    $('#metadata').text("radius:" + randomRadius)
+    setInterval(function(){ 
+
+            number+=1;
+
+            if(number>=colors.length){
+                number=0;
+            }
+
+       $("#shape").css("background-color", colors[number]);
+        $("#name").css("font-size",size[number]);
+        $("#name").css("color", colors1[number]);
+
+
+       
+        var randomText = Math.floor(Math.random() * heys.length);
+        $("#name").html(heys[randomText])
+
+
+        var random = Math.random() * 300  ;
+        var randomCSS =  random  + "px" ;
+       $("#shape").css("height", randomCSS);
+       $("#shape").css("width", randomCSS);
+
+       var width = $(document).width() - random;
+       var height = $(document).height() - random;       
+       
+       var doesnt = Math.random() * width  + "px" ;
+       var does = Math.random() * height + "px" ;
+
+        $("#shape").css("top", does);
+        $("#shape").css("left", doesnt);
+
+
+
+        var random1 = Math.random() * 300  ;
+
+        var width1 = $(document).width() - random1;
+        var height1 = $(document).height() - random1;       
+ 
+       
+        var randomWidth = Math.random() * width1;
+        var randomHeight = Math.random() * height1;
+
+        var leftCss = Math.floor(randomWidth) + "px"
+        var topCss = Math.floor(randomHeight) + "px"
+
+        $("#name").css("left", leftCss)
+        $("#name").css("top", topCss)
+        console.log('hi')
+
+
+
+
+
+}, 1000);
+
+
 
 })
 
-$('#metadata').hover(function(){
 
 
-
-    var randomNumber3 = Math.random() * width;
-    var randomLeft = randomNumber3 + "px"
-
-    var randomNumber4 = Math.random() * height;
-    var randomTop = randomNumber4 + "px"
-
-
-
-    $(this).css('left', randomLeft);
-    $(this).css('top', randomTop);
-
-
-})
-
-
-})
