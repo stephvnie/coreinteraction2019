cb3b1b4adae03d9ba2d75199cff2b79a1f0fcc24
stephvnie <46946099+stephvnie@users.noreply.github.com>
2019-04-16 16:17:15 -0400

    Update index.js

--- exercise4/index.js
+++ exercise4/index.js
@@ -1,34 +1,22 @@
-// console.log("regular js")
-//check if document is ready
 $(document).ready(function(){
-// console.log("jquery")
 
 var name = "exercise 4";
-var colors = ["grey","#00ff00","pink","green"]
+var colors = ["white","white","white","white"]
 
 var time = new Date();
-var ticker = 0;
-var ticker2 = 0;
+var ticker = 1;
+var ticker2 = 1;
 
 
-//change some html
 $('#time').text(time);
 $('#ticker').text(ticker);
 $('#ticker2').text(ticker2);
 $('#name').text(name);
 
 
-//change some css
-$('p').css("font-size",'22px')
-$('p').css("color",'grey')
-
-
-
 document.getElementById('name').style.zIndex = 10;
 
 
-
-//create a function!!!!!!!!111
 function getTime(){
 
     $('#ticker').text(ticker);
@@ -37,42 +25,17 @@ function getTime(){
     $('#time').text(time);
     time = new Date();
 
-
-    var rotate= 'rotate(' + ticker +'deg)';
-    $('#clock').css("transform",rotate);
-    $('#clock').css("background-color",colors[ticker2]);
-
-
-
-
-
-    ticker += 10;
-    ticker2 +=1;
-
-
-
-    if(ticker2 >=4) {
-
-        ticker2=0;
-    }
 }
-//call it
+
+
 getTime();
 
 setInterval(function(){
-    //call it every second
-    // console.log("hey")
+
     getTime();
 },1000)
 
 
 
-//end document ready function
 })
-
-
-
-
-
-
 
