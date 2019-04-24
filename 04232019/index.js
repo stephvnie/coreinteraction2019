04d01049b1821dcdbe61d729d7e8f51d3d80e00a
stephvnie <46946099+stephvnie@users.noreply.github.com>
2019-04-23 21:01:09 -0400

    Update index.js

--- 04232019/index.js
+++ 04232019/index.js
@@ -3,9 +3,19 @@ $(document).ready(function(){
     var width = $(document).width() - 100;
     var height = $(document).height() - 100;
 
+    var txt = "whats goood"
+
+    var colors = ['SteelBlue','DeepSkyBlue','LightSteelBlue','MidnightBlue','RoyalBlue','PowderBlue','MediumBlue','LightBlue','skyblue','Navy','LightSkyBlue'];
+    var BGcolor = ['rgb(116, 106, 82)','olivedrab','dimgrey','cadetblue'];
+
+
+    $(".welcome").text(txt);
+
+
+
 // console.log($(".circle"))
 
-    var shapes = [$(".circle"), $(".triangle"), $(".square")]
+    var shapes = [$(".circle"), $(".triangle"), $(".square"),$(".evian"),$(".evian1"),$(".evian2"),$(".evian3")]
 
 
     setInterval( function randomStuff(){
@@ -15,14 +25,29 @@ $(document).ready(function(){
     // console.log(clone)
     clone.last().css( 'left', Math.random() * width )
     clone.last().css( 'top', Math.random() * height )
-    },100);
+    },800);
 
-random();
    
 
 
 
+    setInterval(function(){ 
 
+           
+    
+           var randomColor = Math.floor(Math.random() * colors.length) ;
+           var randomBGcolor = Math.floor(Math.random() * BGcolor.length) ;
+
+    
+           $(".welcome").css("color",colors[randomColor]);     
+           $("body").css("background-color",BGcolor[randomBGcolor]);     
+
+    
+    
+    
+
+    }, 200);
+    
 
 
 
